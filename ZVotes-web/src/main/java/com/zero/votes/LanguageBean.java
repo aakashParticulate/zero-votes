99b1dcdc5c2bec3d1286a037b08014cc748fa666
Stephan Groß <stephan@minddust.com>
2014-08-11 18:43:49 +0200

    Refactor code

--- ZVotes-web/src/main/java/com/zero/votes/LanguageBean.java
+++ ZVotes-web/src/main/java/com/zero/votes/LanguageBean.java
@@ -1,6 +1,5 @@
 package com.zero.votes;
 
-
 import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.Locale;
@@ -13,16 +12,16 @@ import javax.faces.event.ValueChangeEvent;
  
 @ManagedBean(name="language")
 @SessionScoped
-public class LanguageBean implements Serializable{
+public class LanguageBean implements Serializable {
  
     private static final long serialVersionUID = 1L;
 
     private String localeCode;
 
     private static Map<String,Object> countries;
-    static{
+    static {
         countries = new LinkedHashMap<String,Object>();
-        countries.put("English", Locale.ENGLISH); //label, value
+        countries.put("English", Locale.ENGLISH);
         countries.put("German", Locale.GERMAN);
     }
 
@@ -30,29 +29,22 @@ public class LanguageBean implements Serializable{
         return countries;
     }
 
-
     public String getLocaleCode() {
         return localeCode;
     }
-
 
     public void setLocaleCode(String localeCode) {
         this.localeCode = localeCode;
     }
 
-    //value change event listener
-    public void countryLocaleCodeChanged(ValueChangeEvent e){
+    public void countryLocaleCodeChanged(ValueChangeEvent e) {
 
         String newLocaleValue = e.getNewValue().toString();
 
-        //loop country map to compare the locale code
         for (Map.Entry<String, Object> entry : countries.entrySet()) {
 
-           if(entry.getValue().toString().equals(newLocaleValue)){
-
-                FacesContext.getCurrentInstance()
-                        .getViewRoot().setLocale((Locale)entry.getValue());
-
+           if (entry.getValue().toString().equals(newLocaleValue)) {
+                FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale)entry.getValue());
           }
        }
     }
