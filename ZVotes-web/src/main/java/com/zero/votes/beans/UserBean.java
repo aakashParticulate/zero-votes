9346bb87c1ab2de0b263b713917656fef5a1a9a6
Stephan Groß <stephan@minddust.com>
2014-08-13 17:14:29 +0200

    Add gravatar to backend menu

--- ZVotes-web/src/main/java/com/zero/votes/beans/UserBean.java
+++ ZVotes-web/src/main/java/com/zero/votes/beans/UserBean.java
@@ -10,26 +10,26 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
-@ManagedBean(name="userBean")
+@ManagedBean(name = "userBean")
 @SessionScoped
 public class UserBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     @EJB
     private LdapLogic ldapLogic;
     private LdapUser user;
 
     public LdapUser getUser() {
-            if (user == null) {
-                    ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-                    Principal p = ec.getUserPrincipal();
-                    if (p != null) {
-                            String principalName = p.getName();
-                            user = ldapLogic.getUser(principalName);
-                    }
+        if (user == null) {
+            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+            Principal p = ec.getUserPrincipal();
+            if (p != null) {
+                String principalName = p.getName();
+                user = ldapLogic.getUser(principalName);
             }
-            return user;
+        }
+        return user;
     }
-    
+
 }
