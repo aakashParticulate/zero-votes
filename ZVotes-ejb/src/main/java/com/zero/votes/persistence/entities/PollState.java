71f279fe796ff7f30102efb8eaad8dbb042f1859
Jonas Braun <jbraun@uni-koblenz.de>
2014-08-15 20:50:23 +0200

    PollState to nice Enum Type

--- ZVotes-ejb/src/main/java/com/zero/votes/persistence/entities/PollState.java
+++ ZVotes-ejb/src/main/java/com/zero/votes/persistence/entities/PollState.java
@@ -1,6 +1,18 @@
 package com.zero.votes.persistence.entities;
 
 public enum PollState {
+    PREPARED("Prepared"),
+    STARTED("Started"),
+    VOTING("Voting"),
+    FINISHED("Finished");
 
-    PREPARED, STARTED, VOTING, FINISHED
+    private final String label;
+
+    PollState(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
 }
