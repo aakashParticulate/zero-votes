25d5f2e7f193e00a3dacb3e2ff2928f57ce74edb
Stephan Groß <stephan@minddust.com>
2014-09-11 12:10:03 +0200

    Switch to multicatch

--- ZVotes-web/src/main/java/com/zero/votes/beans/GravatarBean.java
+++ ZVotes-web/src/main/java/com/zero/votes/beans/GravatarBean.java
@@ -22,8 +22,7 @@ public class GravatarBean {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             return hex(md.digest(message.getBytes("CP1252")));
-        } catch (NoSuchAlgorithmException e) {
-        } catch (UnsupportedEncodingException e) {
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
         }
         return null;
     }
